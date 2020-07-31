9ba0cd3fbaea0f7ea6215c2bb3ea779b697d0aac
stelsonip <stelsonip@gmail.com>
2020-07-31 13:51:31 +0200

    update rapidão

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,14 +4,11 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
       <p>
-        Orgulhosamente criado durante a
+        Site criado na #imersãoReact
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+           Alura
         </a>
       </p>
     </FooterBase>
